2ff0086309eeaea4435ee9ef57c611096db024c1
ESTER HADASSA OLIVEIRA LUCHI <25162392@edu119.local>
2025-11-07 16:47:03 -0300

    livraria

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise';
+
+const db = await mysql.createPool({
+  host: 'localhost',
+  user: 'root',       
+  password: 'root',   
+  database: 'dblivraria',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+export default db;
